637df2dbb29281f733191f9b81c791355828efac
sraut <saurabh.raut@blackboard.com>
2017-06-27 15:58:12 +0530

    ngram path

--- .ebextensions/scripts/ngram.sh
+++ .ebextensions/scripts/ngram.sh
@@ -1,10 +1,10 @@
-DIR=/opt/ngram-data/
+DIR=/opt/ngram-data
 
-if [ "$(ls -A $DIR)" ]; then
-     echo "ngran-data already present in $DIR"
+if [ '$(ls -A $DIR)' ] then
+     echo 'ngran-data already present in $DIR'
 
 else
-    echo "$DIR is Empty, Setting up ngram-data, this might take few minutes !"
+    echo '$DIR is Empty, Setting up ngram-data, this might take few minutes !'
     mkdir /opt/ngram-data /opt/ngram-data/en
     chmod 777 -R /opt/ngram-data
     aws s3 cp s3://lt-ngram-data/ngrams-en-20150817.zip /opt/ngram-data/
